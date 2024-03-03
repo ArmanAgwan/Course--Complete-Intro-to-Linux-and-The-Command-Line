a2205d7707e98e23b14520a0a9a9821e6735d7a6
ArmanAgwan <armanagwa@gmail.com>
2024-03-03 19:27:12 +0530

    FPP

---
+++ 03-Files_Pipes&Permissions.js
@@ -0,0 +1,38 @@
+                             //-----Reading Files-----//
+
+// => LESS
+
+
+// :~$  less textfile.txt
+
+// :~$  ls 
+
+// -  textfile.txt  test1.c   test2.c   test3.cpp
+
+// :~$  vi textfile.txt
+
+// :~$  less 
+
+// :~$  less textfile.txt
+
+// :~$  more textfile.txt
+
+
+
+                            //-----Man-----//
+
+
+
+// :~$  man less         // => manual for less.
+
+// :~$  less --help      // => summary of less Commmand
+
+// :~$  man python3
+
+
+
+                        //-----cat-----//
+
+
+
+// :~$ cat textfile.txt           //  Cat does is read the entire file and output It.
